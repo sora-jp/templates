11ece4dcef538d20cf172684fb1d9d6def0353c6
Oskar Tornevall <oskar.tornevall@gmail.com>
2024-09-25 14:49:02 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    default_templates.url = "github:NixOS/templates";
+  };
+  outputs = { self, nixpkgs, utils, default_templates }:
+    let
+      subdirs = nixpkgs.lib.attrsets.filterAttrs
+        (n: v: v == "directory" && builtins.substring 0 1 n != ".")
+        (builtins.readDir ./.);
+      mkTemplate = tdir: _: { "${tdir}" = {path = ./. + tdir; description = "Auto-generated template"; }; };
+      templates = nixpkgs.lib.attrsets.concatMapAttrs mkTemplate subdirs;
+    in
+      (utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+          {
+            devShell = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                nixd
+              ];
+            };
+          }
+      )) // { templates = default_templates.templates // templates; };
+}
